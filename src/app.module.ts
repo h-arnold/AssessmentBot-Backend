c58edd4039327ffb91d34975d00fc04ee95b714d
Hamish Arnold <200~78823477+h-arnold@users.noreply.github.com>
2025-07-17 19:30:41 +0000

    Reverted to working app state in unit tests but retained fixed serialiser.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
-import { ConfigModule } from './config/config.module';
-import { ConfigService } from './config/config.service';
+import { LogRedactor } from './common/utils/log-redactor.util';
 import { AssessorModule } from './v1/assessor/assessor.module';
 
 /**
