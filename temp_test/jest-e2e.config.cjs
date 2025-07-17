a4e13dcd6f95ce393c2e0f9cf5e5fc57c083db9c
Hamish Arnold <78823477+h-arnold@users.noreply.github.com>
2025-07-17 07:49:57 +0000

    chore: add jest-e2e configuration for end-to-end testing setup

---
+++ temp_test/jest-e2e.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/../src/$1',
+  },
+  testRegex: '.+\.e2e-spec\.ts$',
+  setupFiles: ['<rootDir>/../jest.setup.ts'],
+  detectOpenHandles: true,
+};
