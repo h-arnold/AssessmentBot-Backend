ca02f4d5ede8207ab29dc4164e0fb7088df629ff
Hamish Arnold <200~78823477+h-arnold@users.noreply.github.com>
2025-08-08 14:16:31 +0000

    chore: remove unused Jest E2E debug script and verify assessor script

--- scripts/debug-jest-e2e.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Run Jest E2E tests and pipe all output to jest-e2e.log
-node ./node_modules/.bin/jest --runInBand --config jest-e2e.config.cjs "$@" > jest-e2e.log 2>&1
--- scripts/health-check.js
+++ scripts/health-check.js
@@ -1,3 +1,7 @@
+/* This is the health-check script used by the Docker Image
+* It's not linted and doesn't have tests as a result.
+/*
+/* eslint-disable no-console */
 const http = require('http');
 
 const options = {
